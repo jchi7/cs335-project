9b5b5b3f4a58ecdd05d5e62998197f8de5ba1f26
Mark Stevens <mstevens@cs.csubak.edu>
2015-04-24 11:20:44 -0700

    removed output

--- Level.cpp
+++ Level.cpp
@@ -9,16 +9,11 @@ Level::Level()
 Level::Level(int num, int numBasic)
 {
     this->numPlatforms = num;
-    std::cout << "There again1" << std::endl;
     this->objects.reserve(num);
-    std::cout << "There again2" << std::endl;
     this->numBasicEnemies = numBasic;
-    std::cout << "There again3" << std::endl;
     if (this->numBasicEnemies > 0)
         this->enemies.reserve(numBasic);
-    std::cout << "There again4" << std::endl;
     this->currentBasicEnemy = 0;
-    std::cout << "There again5" << std::endl;
 }
 
 Level::~Level()
