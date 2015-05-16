6d9358ada58582eb72d58eb9d0314b2474f57765
Jason Chi <ja5on103@sbcglobal.net>
2015-05-16 13:59:22 -0700

    Got edge detection to work!

--- jasonc.cpp
+++ jasonc.cpp
@@ -5,65 +5,40 @@
 //  This is function is
 //  Arguments:  Take an entity and a platform.
 /////////////////////////////////////////////////
-bool entityPeakAheadDetect(GameObject& ent, Shape* plat)
-{
-    if ((ent.body.center[0] + ent.body.height) <= (plat->center[0] + plat->height))
-        return false;
-    // if near the left edge then you to switch direction
-    if (ent.body.orientation == FACING_RIGHT) {
-       if (ent.body.center[0] + ent.body.width >= plat->center[0] - plat->width &&
-           ent.body.center[0] + ent.body.width <= plat->center[0] + plat->width &&
-           ent.body.center[1] - ent.body.height < plat->center[1] + plat->height &&
-           ent.body.center[1] + ent.body.height > plat->center[1] - plat->height ) {
-           return false;
-        } 
-    } else if (ent.body.orientation == FACING_LEFT) {
-        if (ent.body.center[0] - ent.body.width >= plat->center[0] - plat->width &&
-            ent.body.center[0] - ent.body.width <= plat->center[0] + plat->width &&
-            ent.body.center[1] - ent.body.height < plat->center[1] + plat->height &&
-            ent.body.center[1] + ent.body.height > plat->center[1] - plat->height ) {
-            return false;
-        }
-    }
-    return true;
-}
-
 void enemyPhysics(Game *game)
 {
-    bool isCollision, isEdge;
+    bool isCollision, isEdge, edge;
     Room * current_level = game->getRoomPtr();
 
     for(auto &entity : current_level->enemies) {
         isCollision = false;
-        isEdge = false;
+        isEdge = true;
+        edge = true;
         entity->movement();
         Shape left;
         Shape right;
         left.type = RECTANGLE;
-        left.center[0] = entity->body.center[0] - entity->body.width - 0.5;
-        left.center[1] = entity->body.center[1];
+        left.center[0] = entity->body.center[0] - 2 * entity->body.width;
+        left.center[1] = entity->body.center[1] - 0.1;
         left.center[2] = entity->body.center[2];
         left.width = entity->body.width;
         left.height = entity->body.height;
         right.type = RECTANGLE;
-        right.center[0] = entity->body.center[0] + entity->body.width + 0.5;
-        right.center[1] = entity->body.center[1];
+        right.center[0] = entity->body.center[0] + 2 * entity->body.width;
+        right.center[1] = entity->body.center[1] - 0.1;
         right.center[2] = entity->body.center[2];
         right.width = entity->body.width;
         right.height = entity->body.height;
         for (int i = 0; i < current_level->numPlatforms; i++) {
-            /*if (entityPeakAheadDetect(*entity, &current_level->platforms[i]->body)) {
-                isEdge = true;
-            }*/
             if (entity->body.orientation == FACING_LEFT) {
                 isEdge = collisionRectRect( &left, &current_level->platforms[i]->body);
-                if (isEdge == false) {
-                    //entity->switchDirection();
+                if (isEdge) {
+                    edge = false;
                 }
             } else if (entity->body.orientation == FACING_RIGHT) {
                 isEdge = collisionRectRect( &right, &current_level->platforms[i]->body);
-                if (isEdge == false) {
-                    //entity->switchDirection();
+                if (isEdge) {
+                    edge = false;
                 }
             }
             isCollision = collisionRectRect(&entity->body, &current_level->platforms[i]->body);
@@ -71,6 +46,8 @@ void enemyPhysics(Game *game)
                 entity->onCollision(current_level->platforms[i]);
             }
         }
+        if (edge)
+            entity->switchDirection();
         isCollision = false;
         
         for (int i = 0; i < current_level->numSpikes; i++) {
