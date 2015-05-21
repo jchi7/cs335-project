2d0517140112ecb826993cbef79a8c531b013168
voltron7552 <computersci7552@gmail.com>
2015-05-21 13:28:05 -0700

    audio example

--- collisions.h
+++
@@ -1,8 +0,0 @@
-#ifndef COLLISIONS_H
-#define COLLISIONS_H
-#include "gameObject.h"
-
-
-extern bool collision(gameObject* entity1, gameObject* entity2);
-
-#endif // COLLISIONS_H
--- initializeLevels.h
+++
@@ -1,9 +0,0 @@
-#ifndef INITIALIZELEVELS_H
-#define INITIALIZELEVELS_H
-
-#include "Level.h"
-#include <vector>
-
-Level*** initializeLevels();
-
-#endif // INITIALIZELEVELS_H
